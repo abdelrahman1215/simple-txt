b30e9a578a7229c538b6686661c62ef262a293ac
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-08-11 12:54:16 +0300

    made functions to push and pull the last undone changes

--- src/file_processing/log_changes.c
+++ src/file_processing/log_changes.c
@@ -37,6 +37,48 @@ void free_change_struct(void *change){
     free(change_ptr);
 }
 
+file_change *copy_change(file_change *change_ptr){
+    file_change *ret = malloc(sizeof(file_change));
+    if(ret == NULL) return NULL;
+
+    ret -> change_type = change_ptr -> change_type;
+
+    void *change_info;
+    if(ret -> change_type == addition){
+        file_addition *tmp = malloc(sizeof(file_addition));
+        file_addition *src = (file_addition *)change_ptr -> change_info;
+
+        tmp -> start_line = src -> start_line;
+        tmp -> start_pos = src -> start_pos;
+        tmp -> end_line = src -> end_line;
+        tmp -> end_pos = src -> end_pos;
+
+        change_info = (void *)tmp;
+    }else{
+        file_deletion *tmp = malloc(sizeof(file_deletion));
+        file_deletion *src = (file_deletion *)change_ptr -> change_info;
+
+        tmp -> line_index = src -> line_index;
+        tmp -> position = src -> position;
+
+        size_t len = strlen(src -> deleted_text);
+        tmp -> deleted_text = calloc(len + 1 , sizeof(char));
+        if(tmp == NULL){
+            free(ret);
+
+            return NULL;
+        }
+
+        strncpy(tmp -> deleted_text , src -> deleted_text , len);
+
+        change_info = (void *)tmp;
+    }
+
+    ret -> change_info = change_info;
+
+    return ret;
+}
+
 void push_change(simple_file *file_ptr , file_change change){
     linked_list_add_node(&change , sizeof(file_change) , free_change_struct , file_ptr -> changes_stack);
 
@@ -48,7 +90,28 @@ void push_change(simple_file *file_ptr , file_change change){
 file_change *pull_last_change(simple_file *file_ptr){
     node *tmp = linked_list_get_last_node(file_ptr -> changes_stack);
 
-    file_change *ret = (file_change *)linked_list_get_obj(tmp);
+    file_change *temp = (file_change *)linked_list_get_obj(tmp);
+    file_change *ret = copy_change(temp);
+
+    linked_list_delete_node(linked_list_get_node_no(file_ptr -> changes_stack) - 1 , file_ptr -> changes_stack);
+    free(temp);
+    
+    return ret;
+}
+
+void push_undone_change(simple_file *file_ptr , file_change change){
+    linked_list_add_node(&change , sizeof(file_change) , free_change_struct , file_ptr -> undone_stack);
+}
+
+file_change *pull_last_undone_change(simple_file *file_ptr){
+    node *tmp = linked_list_get_last_node(file_ptr -> undone_stack);
+
+    file_change *temp = (file_change *)linked_list_get_obj(tmp);
+    file_change *ret = copy_change(temp);
+
+    linked_list_delete_node(linked_list_get_node_no(file_ptr -> undone_stack) - 1 , file_ptr -> undone_stack);
+    free(temp);
+
     return ret;
 }
 
