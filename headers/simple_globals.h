63f636af6aa13fd28110b9e47900b25c91a7ca6d
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-08-18 16:34:43 +0300

    defined a current file global

--- headers/simple_globals.h
+++ headers/simple_globals.h
@@ -1,6 +1,10 @@
 #ifndef SIMPLE_GLOBALS
 #define SIMPLE_GLOBALS
 
+#include "simple_file.h"
+
 #define SIMPLE_GLOBAL __attribute__((__common__))
+
+SIMPLE_GLOBAL simple_file *current_file;
 
 #endif
