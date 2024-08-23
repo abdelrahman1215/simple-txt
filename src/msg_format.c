ed3cd70c17eafa194225fc16f4f2d402c3fb943e
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-08-23 23:29:29 +0300

    made a library for messages

---
+++ src/msg_format.c
@@ -0,0 +1,64 @@
+#include "../c_datastructures/headers/linked_list.h"
+#include "../headers/simple_file.h"
+
+#include <string.h>
+#include <stdio.h>
+
+linked_list *msg_queue = NULL;
+
+void init_msg_queue(){
+    if(msg_queue != NULL) return ;
+
+    msg_queue = new_linked_list();
+}
+
+void enqueue_msg(char *msg){
+    if(msg == NULL) return ;
+    if(msg[0] == '\000') return ;
+
+    if(msg_queue == NULL) init_msg_queue();
+
+    size_t len = strlen(msg);
+    linked_list_add_node(msg , len + 1 , NULL , msg_queue);
+}
+
+char *dequeue_msg(){
+    if(msg_queue == NULL) return NULL;
+
+    node *target_node = linked_list_get_first_node(msg_queue);
+    if(target_node == NULL) return NULL;
+
+    char *ret = linked_list_get_obj(target_node);
+    
+    linked_list_delete_node(0 , msg_queue);
+
+    return ret;
+}
+
+void loading_msg(const char *file_name , loading_err error){
+    if(file_name == NULL) return ;
+    if(file_name[0] == '\000') return ;
+
+    char msg[1024];
+    memset(msg , 0 , 1024);
+    switch(error){
+        case File_Not_Found :
+            sprintf(msg , "created new file \"%s\"" , file_name);
+            
+            break;
+
+        default : return ;
+    }
+
+    enqueue_msg(msg);
+}
+
+void wrote_into_file_msg(const char *file_name){
+    if(file_name == NULL) return ;
+    if(file_name[0] == '\000') return ;
+
+    char msg[1024];
+    memset(msg , 0 , 1024);
+
+    sprintf(msg , "wrote the changes into \"%s\"" , file_name);
+}
