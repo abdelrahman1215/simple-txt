edec5f1364669df23011a0a4f8c9d299b7b68a72
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-08-27 18:57:17 +0300

    started on a library for displaying text

---
+++ src/display_txt.c
@@ -0,0 +1,37 @@
+#include "../headers/simple_globals.h"
+#include "../headers/simple_file.h"
+
+#include <pdcurses.h>
+#include <stdlib.h>
+#include <string.h>
+
+void update_text_display(){
+    if(stdscr == NULL) return ;
+    if(Current_File == NULL) return ;
+
+    unsigned int row_no = Txt_Disp_End_Y - Txt_Disp_Start_Y;
+    unsigned int max_len = (Txt_Disp_End_X - Txt_Disp_Start_X);
+    char rows[row_no][max_len + 1];
+    for(unsigned int i = 0 ; i < row_no ; i++){
+        memset(rows[i] , 0 , max_len + 1);
+    }
+
+    char *tmp = NULL;
+    unsigned int used_rows = 0;
+    for(unsigned int i = 0 ; i < row_no ; i++ , used_rows++){
+        tmp = simple_file_get_line(Current_File , i + Txt_Start_Line);
+        if(tmp == NULL) break;
+
+        strncpy(rows[i] , tmp , max_len);
+
+        free(tmp);
+    }
+
+    for(unsigned int i = used_rows ; i < row_no ; i++){
+        rows[i][0] = '~';
+    }
+
+    for(unsigned int i = 0 ; i < row_no ; i++){
+        mvprintw(Txt_Disp_Start_Y + i , Txt_Disp_Start_X , "%s" , rows[i]);
+    }
+}
