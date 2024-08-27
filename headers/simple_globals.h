f8873cebd38e8f5626deacd8e11391e60554ac5b
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-08-27 18:58:07 +0300

    created variables for text displaying

--- headers/simple_globals.h
+++ headers/simple_globals.h
@@ -3,8 +3,12 @@
 
 #include "simple_file.h"
 
+#include <stddef.h>
+
 #define SIMPLE_GLOBAL __attribute__((__common__))
 
 SIMPLE_GLOBAL simple_file *Current_File;
+SIMPLE_GLOBAL size_t Txt_Start_Line;
+SIMPLE_GLOBAL unsigned int Txt_Disp_Start_X , Txt_Disp_Start_Y , Txt_Disp_End_X , Txt_Disp_End_Y;
 
 #endif
