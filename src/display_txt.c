ef73b303333fe52c5c14ece58103141e121c9e2d
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-11 00:22:22 +0300

    added a function that update display values

--- src/display_txt.c
+++ src/display_txt.c
@@ -10,21 +10,58 @@
 int Cursor_X = -1 , Cursor_Y = -1;
 bool display_started = false;
 
+void update_values(){
+    unsigned int disp_width = Txt_Disp_End_X - Txt_Disp_Start_X;
+    size_t line_len = simple_file_get_line_len(Current_File , Line_Pos);
+
+    if(Col_Pos >= Txt_Start_Col + disp_width - Least_H_Distance){
+        Txt_Start_Col = Col_Pos - (disp_width - Least_H_Distance);
+    }else if(Col_Pos <= Txt_Start_Col + Least_H_Distance && Txt_Start_Col > 0){
+        if(Col_Pos < Least_H_Distance){
+            Txt_Start_Col = 0;
+        }else{
+            Txt_Start_Col = Col_Pos - Least_H_Distance;
+        }
+    }
+    
+    if(Col_Pos > line_len){
+        Col_Pos = line_len;
+
+        unsigned int disp_width = Txt_Disp_End_X - Txt_Disp_Start_X;
+        if(Txt_Start_Col > 0 && Txt_Start_Col + Least_H_Distance > line_len){
+            if(Col_Pos > disp_width - Least_H_Distance){
+                Txt_Start_Col = line_len - (disp_width - Least_H_Distance);
+            }else{
+                Txt_Start_Col = 0;
+            }
+        }
+
+        Cursor_X = Txt_Disp_Start_X + (line_len - Txt_Start_Col);
+    }
+
+    Cursor_X = (Col_Pos - Txt_Start_Col) + Txt_Disp_Start_X;
+
+    unsigned int disp_height = Txt_Disp_End_Y - Txt_Disp_Start_Y;
+
+    if(Line_Pos >= Txt_Start_Line + disp_height - Least_V_Distance){
+        Txt_Start_Line = Line_Pos - (disp_height - Least_V_Distance) + 1;
+    }else if(Line_Pos <= Txt_Start_Line + Least_V_Distance && Txt_Start_Line > 0){
+        if(Line_Pos < Least_V_Distance){
+            Txt_Start_Line = 0;
+        }else{
+            Txt_Start_Line = Line_Pos - Least_V_Distance;
+        }
+    }
+
+    Cursor_Y = Txt_Disp_Start_Y + (Line_Pos - Txt_Start_Line);
+}
 
 void move_ncols_right(size_t cols){
     if(display_started == false) return ;
     if(cols == 0) return ;
     if(Col_Pos + cols > simple_file_get_line_len(Current_File , Line_Pos)) return ;
 
-    unsigned int disp_width = Txt_Disp_End_X - Txt_Disp_Start_X;
-
     Col_Pos += cols;
-
-    if(Col_Pos - Txt_Start_Col >= disp_width - Least_H_Distance){
-        Txt_Start_Col = Col_Pos - (disp_width - Least_H_Distance);
-    }
-
-    Cursor_X = (Col_Pos - Txt_Start_Col) + Txt_Disp_Start_X;
 }
 
 void move_ncols_left(size_t cols){
@@ -33,16 +70,6 @@ void move_ncols_left(size_t cols){
     if(cols > Col_Pos) return ;
 
     Col_Pos -= cols;
-
-    if(Col_Pos <= Txt_Start_Col + Least_H_Distance && Txt_Start_Col > 0){
-        if(Col_Pos < Least_H_Distance){
-            Txt_Start_Col = 0;
-        }else{
-            Txt_Start_Col = Col_Pos - Least_H_Distance;
-        }
-    }
-
-    Cursor_X = (Col_Pos - Txt_Start_Col) + Txt_Disp_Start_X;
 }
 
 void move_nlines_down(size_t lines){
@@ -50,31 +77,7 @@ void move_nlines_down(size_t lines){
     if(lines == 0) return ;
     if(Line_Pos + lines >= simple_file_get_line_no(Current_File)) return ;
 
-    unsigned int disp_height = Txt_Disp_End_Y - Txt_Disp_Start_Y;
-
     Line_Pos += lines;
-
-    if(Line_Pos - Txt_Start_Line >= disp_height - Least_V_Distance){
-        Txt_Start_Line = Line_Pos - (disp_height - Least_V_Distance) + 1;
-    }
-
-    Cursor_Y = Txt_Disp_Start_Y + (Line_Pos - Txt_Start_Line);
-
-    size_t line_len = simple_file_get_line_len(Current_File , Line_Pos);
-    if(Col_Pos > line_len){
-        Col_Pos = line_len;
-
-        unsigned int disp_width = Txt_Disp_End_X - Txt_Disp_Start_X;
-        if(Txt_Start_Col > 0 && Txt_Start_Col + Least_H_Distance > line_len){
-            if(Col_Pos > disp_width - Least_H_Distance){
-                Txt_Start_Col = line_len - (disp_width - Least_H_Distance);
-            }else{
-                Txt_Start_Col = 0;
-            }
-        }
-
-        Cursor_X = Txt_Disp_Start_X + (line_len - Txt_Start_Col);
-    }
 }
 
 void move_nlines_up(size_t lines){
@@ -83,32 +86,6 @@ void move_nlines_up(size_t lines){
     if(Line_Pos == 0) return ;
 
     Line_Pos -= lines;
-
-    if(Line_Pos <= Txt_Start_Line + Least_V_Distance && Txt_Start_Line > 0){
-        if(Line_Pos < Least_V_Distance){
-            Txt_Start_Line = 0;
-        }else{
-            Txt_Start_Line = Line_Pos - Least_V_Distance;
-        }
-    }
-    
-    Cursor_Y = Txt_Disp_Start_Y + (Line_Pos - Txt_Start_Line);
-
-    size_t line_len = simple_file_get_line_len(Current_File , Line_Pos);
-    if(Col_Pos > line_len){
-        Col_Pos = line_len;
-
-        unsigned int disp_width = Txt_Disp_End_X - Txt_Disp_Start_X;
-        if(Txt_Start_Col > 0 && Txt_Start_Col + Least_H_Distance > line_len){
-            if(Col_Pos > disp_width - Least_H_Distance){
-                Txt_Start_Col = line_len - (disp_width - Least_H_Distance);
-            }else{
-                Txt_Start_Col = 0;
-            }
-        }
-
-        Cursor_X = Txt_Disp_Start_X + (line_len - Txt_Start_Col);
-    }
 }
 
 void move_to_pos(size_t line_index , size_t col_index){
@@ -139,6 +116,7 @@ void update_text_display(){
     if(stdscr == NULL) return ;
 
     if(Current_File == NULL) return ;
+    update_values();
 
     unsigned int row_no = Txt_Disp_End_Y - Txt_Disp_Start_Y;
     unsigned int max_len = Txt_Disp_End_X - Txt_Disp_Start_X;
@@ -166,6 +144,7 @@ void update_text_display(){
     for(unsigned int i = used_rows ; i < row_no ; i++){
         rows[i][0] = '~';
     }
+
 
     for(unsigned int i = 0 ; i < row_no ; i++){
         mvprintw(Txt_Disp_Start_Y + i , Txt_Disp_Start_X , "%s" , rows[i]);
