5f7409cb774548ba783ff6ac40656f2d7a076f74
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-12 21:57:54 +0300

    joined file_pos.h and display_txt.h into display.h

--- headers/display.h
+++ headers/display.h
@@ -1,10 +1,12 @@
-#ifndef FILE_POS
-#define FILE_POS
+#ifndef DISPLAY_H
+#define DISPLAY_H
 
 void move_ncols_right(size_t cols);
 void move_ncols_left(size_t cols);
 void move_nlines_down(size_t lines);
 void move_nlines_up(size_t lines);
 void move_to_pos(size_t line_index , size_t col_index);
+
+void update_text_display();
 
 #endif
--- headers/display_txt.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_TXT
-#define DISPLAY_TXT
-
-void update_text_display();
-
-#endif
