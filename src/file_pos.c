bd686f03700d53810411ef87d9856988d4f3db67
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-12 20:12:22 +0300

    made it so that if the cursor is at the end of the line and the cursor is moved to the right it moves to the start of the next line and vice versa

--- src/file_pos.c
+++ src/file_pos.c
@@ -4,14 +4,33 @@
 
 void move_ncols_right(size_t cols){
     if(cols == 0) return ;
-    if(Col_Pos + cols > simple_file_get_line_len(Current_File , Line_Pos)) return ;
+    if(Col_Pos + cols > simple_file_get_line_len(Current_File , Line_Pos)){
+        if(cols > 1) return ;
+    }
 
-    Col_Pos += cols;
+    if(cols == 1 && Col_Pos == simple_file_get_line_len(Current_File , Line_Pos)){
+        if(Line_Pos == simple_file_get_line_no(Current_File)) return ;
+
+        Line_Pos++;
+        Col_Pos = 0;
+    }else{
+        Col_Pos += cols;
+    }
+
 }
 
 void move_ncols_left(size_t cols){
     if(cols == 0) return ;
-    if(cols > Col_Pos) return ;
+    if(cols > Col_Pos){
+        if(cols > 1) return ; 
+    }
+
+    if(cols == 1 && Col_Pos == 0){
+        if(Line_Pos == 0) return ;
+
+        Line_Pos --;
+        Col_Pos = simple_file_get_line_len(Current_File , Line_Pos) + 1;
+    }
 
     Col_Pos -= cols;
 }
