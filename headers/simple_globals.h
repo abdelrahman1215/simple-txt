be0e32a7f906e5b35909021e481a8145f06214bb
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-12 13:23:40 +0300

    dd

--- headers/simple_globals.h
+++ headers/simple_globals.h
@@ -14,11 +14,12 @@
 
 #define TEXT 1
 #define SIDE_STRIPS 2
-#define UPPER_STRIP 3
+#define BACKGROUND 3
 
 SIMPLE_GLOBAL simple_file *Current_File;
 SIMPLE_GLOBAL unsigned int Txt_Disp_Start_X , Txt_Disp_Start_Y , Txt_Disp_End_X , Txt_Disp_End_Y;
 SIMPLE_GLOBAL unsigned int Least_H_Distance , Least_V_Distance;
+SIMPLE_GLOBAL unsigned int Screen_Width , Screen_Height;
 SIMPLE_GLOBAL size_t Col_Pos , Line_Pos;//to know our position in the file
 
 #endif
