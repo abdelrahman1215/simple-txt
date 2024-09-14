1acbb74f314835fd05e2670da024067195314338
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-14 07:12:04 +0300

    remove the display_file_name function to display_txt.c as it is now only used by it (in case display_file_name is true)

--- headers/display.h
+++ headers/display.h
@@ -1,11 +1,9 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-void update_values();
+#include <stdbool.h>
 
 void init_color_pairs();
-
-void display_file_name();
 
 void move_ncols_right(size_t cols);
 void move_ncols_left(size_t cols);
@@ -13,7 +11,7 @@ void move_nlines_down(size_t lines);
 void move_nlines_up(size_t lines);
 void move_to_pos(size_t line_index , size_t col_index);
 
-void update_text_display();
+void update_text_display(simple_file *file_ptr , bool display_line_no , bool display_file_name , unsigned int start_x , unsigned int end_x , unsigned int start_y , unsigned end_y);
 
 void render_background();
 
--- src/display/display.c
+++ src/display/display.c
@@ -1,5 +1,4 @@
 #include "display_txt.c"
-#include "file_pos.c"
 
 #include <string.h>
 
@@ -12,41 +11,6 @@ void render_background(){
     }
 
     attroff(COLOR_PAIR(BACKGROUND));
-}
-
-void display_file_name(){
-    if(Current_File == NULL || Txt_Disp_Start_Y == 0) return ;
-
-    char *file_path;
-    {
-        const char *tmp = (char *)simple_file_get_name(Current_File);
-        size_t path_len = strlen(tmp) + 1;
-
-        file_path = calloc(path_len + 1 , sizeof(char));
-        strncpy(file_path , tmp , path_len);
-    }
-
-    char *file_name = strtok(file_path , "/\\");
-    for(char *tmp = strtok(NULL , "/\\") ; tmp != NULL ; file_name = tmp , tmp = strtok(NULL , "/\\")){}
-    free(file_path);
-
-    size_t name_len = strlen(file_name);
-    if(name_len + 6 > (Txt_Disp_End_X - Text_Start_X)){
-        if((Txt_Disp_End_X - Text_Start_X) < 6){
-            return;
-        }
-        file_name[(Txt_Disp_End_X - Text_Start_X) - 6] = '\000';
-    }
-
-    attron(COLOR_PAIR(TITLE));
-
-    curs_set(0);
-
-    mvprintw(Txt_Disp_Start_Y - 1 , Text_Start_X + ((Txt_Disp_End_X - Text_Start_X)/2) - (strlen(file_name)/2) , "%s" , file_name);
-    
-    curs_set(1);
-
-    attroff(COLOR_PAIR(TITLE));
 }
 
 void init_color_pairs(){
