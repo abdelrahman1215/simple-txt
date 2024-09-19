d508aec9120b8ac32ccf7db63fbfe69cbaf8c0ac
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-09-19 22:31:49 +0300

    added an option in render_background to choose the color pair that will be used

--- headers/display.h
+++ headers/display.h
@@ -9,6 +9,8 @@ void init_color_pairs();
 
 void update_text_display(simple_file *file_ptr , bool display_line_no , bool display_file_name , bool highlight_current_line  , bool scroll , unsigned int start_x , unsigned int end_x , unsigned int start_y , unsigned end_y);
 
-void render_background(unsigned int start_x , unsigned int start_y , unsigned int end_x , unsigned int end_y);
+void render_background(unsigned int start_x , unsigned int start_y , unsigned int end_x , unsigned int end_y , int color_pair);
+
+void update_lower_strip();
 
 #endif
