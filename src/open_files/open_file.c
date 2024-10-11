9a8de2ef9a5d0455edba239d97408f392df70607
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-10-11 10:12:37 +0300

    dd

--- src/open_files/open_file.c
+++ src/open_files/open_file.c
@@ -15,15 +15,9 @@ void open_file(char *file_name){
             save_file(Current_File);
             destroy_simple_file(Current_File);
         }
-        
+
         Current_File = tmp;
     }
 
-    if(error == OK || error == File_Not_Found){
-        if(error == File_Not_Found){
-            loading_msg(file_name , error);
-        }
-    }else{
-        loading_error(file_name , error);
-    }
+    loading_error(file_name , error);
 }
