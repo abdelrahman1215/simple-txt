3a039c721aac3cbad865b98a6b351328440b9bf6
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-10-11 10:10:33 +0300

    made the edit function use the open_file function so that opening files would have the same behaviour either it was from the terminal of using the edit command

--- src/commands/commands.c
+++ src/commands/commands.c
@@ -1,6 +1,7 @@
 #include "../../headers/simple_globals.h"
 #include "../../headers/simple_file.h"
 #include "../../headers/msg_fmt.h"
+#include "../../headers/open_files.h"
 
 #include <stdlib.h>
 
@@ -35,18 +36,7 @@ void write_and_quit(char **args){
 //command : "edit"
 //tokens_no : 1
 void edit_file(char **args){
-    loading_err error;
-    simple_file *tmp = load_file(args[0] , true , &error);
-
-    if(error == OK || error == File_Not_Found){
-        save_file(Current_File);
-        destroy_simple_file(Current_File);
-        Current_File = tmp;
-
-        if(error == File_Not_Found){
-            loading_msg(args[0] , File_Not_Found);
-        }
-    } else loading_error(args[0] , error);
+    open_file(args[0]);
 
     return ;
 }
--- src/open_files/open_file.c
+++ src/open_files/open_file.c
@@ -8,7 +8,16 @@ void open_file(char *file_name){
     if(file_name[0] == '\000') return ;
 
     loading_err error;
-    Current_File = load_file(file_name , true , &error);
+    simple_file *tmp = load_file(file_name , true , &error);
+
+    if(tmp != NULL){
+        if(Current_File != NULL){
+            save_file(Current_File);
+            destroy_simple_file(Current_File);
+        }
+        
+        Current_File = tmp;
+    }
 
     if(error == OK || error == File_Not_Found){
         if(error == File_Not_Found){
