73b189b9e9daeef8d7277f954015fc804055f7c7
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-10-23 01:35:37 +0300

    cc
    made a chnage to update text so that update text would increase the start col
    only if the line length is greater that the text display width to give a sense to
    the user if a line's length is equal to the width if the text display

--- src/display/display_txt.c
+++ src/display/display_txt.c
@@ -31,7 +31,7 @@ void update_values(text_display_info *info_ptr){
     static simple_file *last_displayed_file = NULL;
 
     info_ptr -> line_pos = simple_file_get_curr_line(info_ptr -> file) , info_ptr -> col_pos = simple_file_get_curr_column(info_ptr -> file);
-    //size_t line_len = simple_file_get_line_len(info_ptr -> file , info_ptr -> line_pos);
+    size_t line_len = simple_file_get_line_len(info_ptr -> file , info_ptr -> line_pos);
     size_t line_no = simple_file_get_line_no(info_ptr -> file);
 
     if(info_ptr -> display_line_no){
@@ -47,7 +47,7 @@ void update_values(text_display_info *info_ptr){
     unsigned int disp_height = info_ptr -> disp_end_y - info_ptr -> txt_start_y;
     unsigned int disp_width = info_ptr -> disp_end_x - info_ptr -> txt_start_x;
 
-    if(info_ptr -> col_pos >= info_ptr -> start_col + disp_width - info_ptr -> least_h_dist){
+    if(info_ptr -> col_pos >= info_ptr -> start_col + disp_width - info_ptr -> least_h_dist && line_len > disp_width){
         info_ptr -> start_col = info_ptr -> col_pos - (disp_width - info_ptr -> least_h_dist);
     }else if(info_ptr -> col_pos <= info_ptr -> start_col + info_ptr -> least_h_dist && info_ptr -> start_col > 0){
         if(info_ptr -> col_pos < info_ptr -> least_h_dist){
