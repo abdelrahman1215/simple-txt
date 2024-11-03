7c28c3cb838e6b4c313e60c6633a4a6f328d286f
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-11-03 21:33:06 +0200

    dd

--- src/input/command_mode.c
+++ src/input/command_mode.c
@@ -50,11 +50,13 @@ autocomp_info autocomplete(char *input , size_t pos , bool get_replacement){
 
             command_len = strlen(target_info -> command);
             if(command_len == tok_len && i == 0){
-                ret.rep_options[ret.opt_no - 1] = calloc(command_len + 1 , sizeof(char));
+                ret.rep_options[ret.opt_no - 1] = calloc(command_len + 2 , sizeof(char));
                 strncpy(ret.rep_options[ret.opt_no - 1] , target_info -> command , command_len);   
+                ret.rep_options[ret.opt_no - 1][command_len] = ' ';
             }else{
-                ret.rep_options[i] = calloc(command_len + 1 , sizeof(char));
+                ret.rep_options[i] = calloc(command_len + 2 , sizeof(char));
                 strncpy(ret.rep_options[i] , target_info -> command , command_len);
+                ret.rep_options[i][command_len] = ' ';
                 i++;
             }
 
