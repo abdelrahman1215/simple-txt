e99fa2af5f0a13e1f16e3c56f21c0e0eb036542d
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2025-01-02 15:30:34 +0200

    made dir_name and file_name into constants

--- headers/open_files.h
+++ headers/open_files.h
@@ -4,7 +4,7 @@
 #include "curses_header.h"
 
 
-void open_file(char *file_name);
-void open_dir(char *dir_name , WINDOW *disp_window , unsigned int start_x , unsigned int end_x , unsigned int start_y , unsigned int end_y);
+void open_file(const char *file_name);
+void open_dir(const char *dir_name , WINDOW *disp_window , unsigned int start_x , unsigned int end_x , unsigned int start_y , unsigned int end_y);
 
 #endif
